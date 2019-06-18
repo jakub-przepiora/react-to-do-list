30896750966c36a5321c3203af2a72bdc13d81b4
Jakub Przepióra <42652452+jakub-przepiora@users.noreply.github.com>
2019-06-19 01:44:44 +0200

    CSS and working addTask

--- src/App.js
+++ src/App.js
@@ -2,41 +2,66 @@ import React from 'react';
 
 import './App.css';
 
-const tasks = [];
+// const tasks = [];
 
-tasks.push("learn react");
-tasks.push("Go shopping");
-tasks.push("buy flowers");
-
+// tasks.push("learn react");
+// tasks.push("Go shopping");
+// tasks.push("buy flowers");
+class ToDoItems extends React.Component {
+  
+  render() { 
+    
+  var todoEntries = this.props.entries;
+    
+    function createTasks(task){
+      return <li key={task.key}>{task.value} <button>Remove</button></li>
+    }
+    
+    var listTasks = todoEntries.map(createTasks);
+    
+    return (
+      <div>
+        <ul className="theList">
+          {listTasks}
+        </ul>
+      </div>
+    );
+  }
+}
 class App extends React.Component {
   
   constructor(props){
     super(props);
     this.state ={
-      value: ""
+      value: "",
+      tasks: []
     }
-    this.addItem = this.addItem.bind(this);
+    // this.addItem = this.addItem.bind(this);
     this.changer = this.changer.bind(this);
   }
   changer(e) {
     this.setState({value: e.target.value});
   }
-  addItem() {
-      console.log(this.state.value);
-      // this.refs.form.reset();
-   return tasks.push(this.state.value);
-      
+  addItem(e) {
+    var itemArray = this.state.tasks;
+    itemArray.push({
+      value: this.state.value,
+      key: Date.now()
+    });
 
+    this.setState({
+      tasks: itemArray
+    });
+    this.state.value = "";
+    e.preventDefault();
   }
   
   render() {
     return (
       
-      <div>
-        <ul>
-          {tasks.map(task => <li key={task}>{task}</li>)}
-        </ul>
-        <form onSubmit={this.addItem}>
+      <div className="container">
+        <ToDoItems entries={this.state.tasks} />
+        <form onSubmit={this.addItem.bind(this)}>
           <input type="text" placeholder="Task" value={this.state.value} onChange={this.changer}  />
           <button type="submit">Add!</button>
         </form>
