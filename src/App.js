e8c0698814e9e64584b4f9bb9f7377a5ff0eae37
Jakub Przepióra <42652452+jakub-przepiora@users.noreply.github.com>
2019-06-20 21:26:53 +0200

    valid

--- src/App.js
+++ src/App.js
@@ -2,16 +2,11 @@ import React from 'react';
 
 import './App.css';
 
-// const tasks = [];
-
-// tasks.push("learn react");
-// tasks.push("Go shopping");
-// tasks.push("buy flowers");
 class ToDoItems extends React.Component {
   
   render() { 
     
-  var todoEntries = this.props.entries;
+    var todoEntries = this.props.entries;
     
     function createTasks(task){
       return <li key={task.key}>{task.value} <button>Remove</button></li>
@@ -28,32 +23,56 @@ class ToDoItems extends React.Component {
     );
   }
 }
+
+// class ErrorLog extends React.Component{
+  
+//   render(){
+//     return(
+//       <p>{error}</p>
+//     );
+//   }
+// }
+
 class App extends React.Component {
   
   constructor(props){
     super(props);
     this.state ={
       value: "",
-      tasks: []
+      tasks: [],
+      error: "",
     }
-    // this.addItem = this.addItem.bind(this);
     this.changer = this.changer.bind(this);
   }
+ 
   changer(e) {
     this.setState({value: e.target.value});
   }
   addItem(e) {
-    var itemArray = this.state.tasks;
-    itemArray.push({
-      value: this.state.value,
-      key: Date.now()
-    });
+    
+    if(this.state.value){
+      var itemArray = this.state.tasks;
+      itemArray.push({
+        value: this.state.value,
+        key: Date.now()
+      });
+     
+      this.setState({
+        tasks: itemArray,
+        error: ""
+      });
 
-    this.setState({
-      tasks: itemArray
-    });
-    this.state.value = "";
-    e.preventDefault();
+      this.state.value = "";
+      e.preventDefault();
+    }
+    else{
+      this.setState({
+        error: "Empty field"
+      })
+      this.state.value = "";
+      e.preventDefault();
+     
+    }
   }
   
   render() {
@@ -61,11 +80,12 @@ class App extends React.Component {
       
       <div className="container">
         <ToDoItems entries={this.state.tasks} />
+        <p>{this.state.error}</p>
         <form onSubmit={this.addItem.bind(this)}>
           <input type="text" placeholder="Task" value={this.state.value} onChange={this.changer}  />
           <button type="submit">Add!</button>
         </form>
-        
+
       </div>
     );
   }
